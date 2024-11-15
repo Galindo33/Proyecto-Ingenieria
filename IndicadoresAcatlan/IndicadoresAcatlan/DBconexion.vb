5518b576bd3bcf4fc82262923a6326eae0cb246f
Galindo33 <316183154@pcpuma.acatlan.unam.mx>
2024-11-14 21:13:33 -0600

    Add files via upload

---
+++ IndicadoresAcatlan/IndicadoresAcatlan/DBconexion.vb
@@ -0,0 +1,41 @@
+﻿'antes de ejecutar se debera configurar de manera
+'adecuada la conexion con la base de datos
+'de lo contrario marca error
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.IO
+
+
+Public Class DBconexion
+
+    Dim conexion As New SqlConnection()
+    Dim direccion As ConnectionStringSettings
+    Dim comando As SqlCommand
+
+    Sub conectar()
+
+        'obtener nombre de la conexion desde el app config
+        direccion = ConfigurationManager.ConnectionStrings("nombre de la conexion")
+        conexion.ConnectionString = direccion.ConnectionString
+        conexion.Open()
+        MsgBox("CONEXION EXITOSA")
+
+    End Sub
+
+    Sub desconectar()
+
+        conexion.Close()
+
+    End Sub
+
+    Sub insertar(logro As Double, causa As String, plnAcn As String)
+
+        conectar()
+        comando = New SqlCommand("insert into database values(" + logro + "," + causa + "," + plnAcn + ")", conexion)
+        comando.ExecuteNonQuery()
+        desconectar()
+
+    End Sub
+
+End Class
---
+++ IndicadoresAcatlan/IndicadoresAcatlan/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.IO
+
+Public Class Form1
+
+    Dim dbconx As New DBconexion
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    'asegurarse de que el metodo se usa con el nombre del boton que guarda los datos
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+
+        'asegurarse de que el metodo se ejecute con el mismo numero de variables 
+        'que cajas de texto en el formulario.
+        'asegurarse de que el metodo utiliza los nombres correctos de las
+        'cajas de texto en el formulario
+        DBconexion.insertar(txt_log.Text, txt_causa.Text, txt_plnAcn.Text)
+
+    End Sub
+
+End Class
