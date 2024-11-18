985d25b31bea6277534a65340341cb28990ea514
Galindo33 <316183154@pcpuma.acatlan.unam.mx>
2024-11-17 22:48:08 -0600

    Add files via upload
    Esta es la actualización de el avance pasado 
    Funciones de validacion, alamcenamiento

---
+++ IndicadoresAcatlan/IndicadoresAcatlan/DBconexion.vb
@@ -0,0 +1,77 @@
+﻿'antes de ejecutar se debera configurar de manera
+'adecuada la conexion con la base de datos
+'de lo contrario marca error.
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.IO
+
+
+Public Class DBconexion
+
+    Dim log As Double
+    Dim causa As String
+    Dim plnAcn As String
+    Dim proy As Double
+
+
+    Dim conexion As New SqlConnection()
+    Dim direccion As ConnectionStringSettings
+    Dim comando As SqlCommand
+
+    Sub Conectar()
+
+        'obtener nombre de la conexion desde el app config.
+        direccion = ConfigurationManager.ConnectionStrings("nombre de la conexion")
+        conexion.ConnectionString = direccion.ConnectionString
+        conexion.Open()
+        MsgBox("CONEXION EXITOSA")
+
+    End Sub
+
+    Sub Desconectar()
+
+        conexion.Close()
+
+    End Sub
+
+    'metodo que almacena en sql la informacion ingresada por el usuario.
+    Sub Insertar(logro, causa, plnAcn)
+
+        Conectar()
+        comando = New SqlCommand("insert into database values(" + logro + "," + causa + "," + plnAcn + ")", conexion)
+        comando.ExecuteNonQuery()
+        desconectar()
+
+    End Sub
+
+    'metodo que valida el valor de la casilla <logro>
+    'ingresado por el usuario.
+    Sub Validacion(logro, proy)
+
+        'si el porcentaje del logro con respecto a la proyeccion
+        'es mayor a 80, las casillas de causa y plan de accion
+        'no se habilitan para su escritura.
+        If (logro * 100) / proy >= 80 Then
+
+            'txtTarget1 ytxtTarget2 hacen referencia a los nombres
+            'de las cajas de texto para causa y plan.
+            'asegurarse de usar los nombres correctos.
+            txtTarget1.Enabled = False
+            txtTarget2.Enabled = False
+
+            'si el porcentaje del logro con respecto a la proyeccion
+            'es menor a 80, las casillas de causa y plan de accion
+            'se habilitan para su escritura.
+        ElseIf (logro * 100) / proy <= 80 And (logro * 100) / proy >= 70 Then
+
+            'txtTarget1 ytxtTarget2 hacen referencia a los nombres
+            'de las cajas de texto para causa y plan.
+            'asegurarse de usar los nombres correctos.
+            txtTarget1.Enabled = True
+            txtTarget2.Enabled = True
+
+        End If
+    End Sub
+
+End Class
---
+++ IndicadoresAcatlan/IndicadoresAcatlan/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.IO
+
+Public Class Form1
+    'variables para almacenar los valores
+    'de las cajas de texto.
+    Dim log As Double
+    Dim causa As String
+    Dim planactn As String
+    Dim proy As Double
+
+    Dim dbconx As New DBconexion()
+
+    'se asignan los valores de las cajas
+    'a las variables.
+    'asegurarse de que se utilizan los nombres correctos de las
+    'cajas de texto en el formulario.
+    log = logro.Text
+    causa = causa.Text
+    planactn = plan.Text
+    proy = proy.Text
+
+    
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    'evento que manda a llamar a la funcion <validar> una vez que el usuario 
+    'ingresa informacion en la casilla de <logro>.
+    'asegurarse de que el evento se usa con el nombre de la casilla <logro>.
+    Private Sub txtTargetControl(sender As Object, e As EventArgs) Handles txtControl.TextChanged
+
+        dbconx.Validacion(log, proy)
+
+    End Sub
+
+
+    'evento que manda a llamar a la funcion <insertar> una vez que el usuario 
+    'hace click en el boton de guargar informacion.
+    'asegurarse de que el evento se usa con el nombre del boton que guarda los datos.
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+
+        'asegurarse de que el metodo se ejecute con el mismo numero de variables 
+        'que cajas de texto en el formulario.
+        dbconx.Insertar(log, causa, planactn)
+
+    End Sub
+
+
+
+
+End Class
